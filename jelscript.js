c16c784616d1d31d2e7b2e36d8eec657d54e5f81
Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2022-01-20 08:18:43 +0700

    Add files via upload

--- jelscript.js
+++ jelscript.js
@@ -1,83 +1,97 @@
-function jelscript(input, storage, balance) {
-	const instructions = input.replace(/\t/g, "").split("\n").filter(ins => ins !== "");
+function jelscript(input, storage, balance, userArgs, address, blockInfo) {
+	const instructions = input.trim().replace(/\t/g, "").split("\n").filter(ins => ins !== "");
 
 	const memory = {};
+
+	userArgs = userArgs.map(arg => arg.toString());
 
 	let ptr = 0;
 
 	while (ptr < instructions.length && balance >= 0) {
-		const line = instructions[ptr];
+		const line = instructions[ptr].trim();
 		const command = line.split(" ").filter(tok => tok !== "")[0];
 		const args = line.slice(command.length + 1).replace(/\s/g, "").split(",").filter(tok => tok !== "");
 
 		switch (command) {
 			case "set":
-				memory[args[0]] = getValue(args[1], memory);
+				memory[args[0]] = getValue(args[1], memory, userArgs);
+				break;
+			case "balance":
+				memory[args[0]] = balance.toString();
+				break;
+			case "address":
+				memory[args[0]] = address;
+				break;
+			case "timestamp":
+				memory[args[0]] = blockInfo.timestamp;
+				break;
+			case "difficulty":
+				memory[args[0]] = blockInfo.difficulty;
 				break;
 			case "store":
-				storage[args[0]] = getValue(args[1], memory);
+				storage[getValue(args[0], memory, userArgs)] = getValue(args[1], memory, userArgs);
 				break;
 			case "pull":
-				memory[args[0]] = storage[args[1]];
+				memory[args[0]] = storage[getValue(args[1], memory, userArgs)] ? storage[getValue(args[1], memory, userArgs)] : "0";
 				break;
 			case "jump":
-				if (getValue(args[0], memory) === "1") {
-					ptr = instructions.indexOf(instructions.find(line => line.startsWith("label " + getValue(args[1], memory))));
+				if (getValue(args[0], memory, userArgs) === "1") {
+					ptr = instructions.indexOf(instructions.find(line => line.startsWith("label " + getValue(args[1], memory, userArgs))));
 				}
 				break;
 			case "add":
-				memory[args[0]] = (parseInt(memory[args[0]]) + parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) + parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "sub":
-				memory[args[0]] = (parseInt(memory[args[0]]) - parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) - parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "mul":
-				memory[args[0]] = (parseInt(memory[args[0]]) * parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) * parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "div":
-				memory[args[0]] = (parseInt(memory[args[0]]) / parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) / parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "mod":
-				memory[args[0]] = (parseInt(memory[args[0]]) % parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) % parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "and":
-				memory[args[0]] = (parseInt(memory[args[0]]) & parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) & parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "or":
-				memory[args[0]] = (parseInt(memory[args[0]]) | parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) | parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "xor":
-				memory[args[0]] = (parseInt(memory[args[0]]) ^ parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) ^ parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "not":
 				memory[args[0]] = (~parseInt(memory[args[0]])).toString();
 				break;
 			case "gtr":
-				memory[args[0]] = parseInt(memory[args[0]]) > parseInt(getValue(args[1], memory)) ? "1" : "0";
+				memory[args[0]] = parseInt(memory[args[0]]) > parseInt(getValue(args[1], memory, userArgs)) ? "1" : "0";
 				break;
 			case "lss":
-				memory[args[0]] = parseInt(memory[args[0]]) < parseInt(getValue(args[1], memory)) ? "1" : "0";
+				memory[args[0]] = parseInt(memory[args[0]]) < parseInt(getValue(args[1], memory, userArgs)) ? "1" : "0";
 				break;
 			case "geq":
-				memory[args[0]] = parseInt(memory[args[0]]) >= parseInt(getValue(args[1], memory)) ? "1" : "0";
+				memory[args[0]] = parseInt(memory[args[0]]) >= parseInt(getValue(args[1], memory, userArgs)) ? "1" : "0";
 				break;
 			case "leq":
-				memory[args[0]] = parseInt(memory[args[0]]) <= parseInt(getValue(args[1], memory)) ? "1" : "0";
+				memory[args[0]] = parseInt(memory[args[0]]) <= parseInt(getValue(args[1], memory, userArgs)) ? "1" : "0";
 				break;
 			case "equ":
-				memory[args[0]] = parseInt(memory[args[0]]) === parseInt(getValue(args[1], memory)) ? "1" : "0";
+				memory[args[0]] = parseInt(memory[args[0]]) === parseInt(getValue(args[1], memory, userArgs)) ? "1" : "0";
 				break;
 			case "neq":
-				memory[args[0]] = parseInt(memory[args[0]]) !== parseInt(getValue(args[1], memory)) ? "1" : "0";
+				memory[args[0]] = parseInt(memory[args[0]]) !== parseInt(getValue(args[1], memory, userArgs)) ? "1" : "0";
 				break;
 			case "ls":
-				memory[args[0]] = (parseInt(memory[args[0]]) << parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) << parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "rs":
-				memory[args[0]] = (parseInt(memory[args[0]]) >> parseInt(getValue(args[1], memory))).toString();
+				memory[args[0]] = (parseInt(memory[args[0]]) >> parseInt(getValue(args[1], memory, userArgs))).toString();
 				break;
 			case "log":
-				console.log(getValue(args[0], memory));
+				console.log(getValue(args[0], memory, userArgs));
 		}
 
 		ptr++;
@@ -87,7 +101,7 @@ function jelscript(input, storage, balance) {
 	return [storage, balance];
 }
 
-function getValue(token, memory) {
+function getValue(token, memory, userArgs) {
 	if (token.startsWith("$")) {
 		token = token.replace("$", "");
 
@@ -96,6 +110,10 @@ function getValue(token, memory) {
 		}
 
 		return memory[token];
+	} else if (token.startsWith("%")) {
+		token = token.replace("%", "");
+
+		return typeof userArgs[parseInt(token)] === "undefined" ? "0" : userArgs[parseInt(token)];
 	} else {
 		return token;
 	}
